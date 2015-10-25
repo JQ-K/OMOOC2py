690d5ab7e549579dbcfabd0ab92b7d08756b49fd
kangqingjie01@gmail.com <kangqingjie01@gmail.com>
2015-10-25 14:13:14 +0800

    second

--- _src/om2py0w/0wex1/main.py
+++ _src/om2py0w/0wex1/main.py
@@ -4,16 +4,22 @@ import time #导入time 模块
 import os
   
 script, filename = argv
-os.path.isfile(filename)  
-# txt = open(filename) #读取内容需先将其打开
+E = os.path.exists(filename) #判断filename是否存在 
 
-print "这是 %r 的内容:" % filename
-# print txt.read() #读取内容
+if str(E) == 'True': #str 可将数据转化为字符串
+    txt = open(filename) #读取内容需先将其打开
+    print '这是 %r 的内容：' % filename 
+    print txt.read()  # 每次运行自动打印之前的记录
+else:
+    print '尚无 %r ---创建之...' % filename
+    
+    # ---以下输入内容并保存---
+    
 
 print "If you don't want that, hit CTRL-C (^C)."
 print "IF you do want this, hit return."
 
-raw_input("?")
+raw_input('> ')
     
 print "Opening the file..."
 target = open(filename,"a") # 'a' 表示要向文件写入数据，但是添加到当前内容尾部
@@ -29,7 +35,7 @@ print T
 
 print "I'm going to write these to the file."
 
-target.write('\n' + content + '\n' + "---"+ T)
+target.write('\n' + content + '\n' +  "---"+ T)
 
 
 print "And finally, we close it."
